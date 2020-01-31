f810adba80d9ae656ac7ffe9c61ed5d4b9fb4bfd
Morgana Oliveira <morganaboos@hotmail.com>
2020-01-31 14:50:03 -0300

    O curso é muito Brabo

---
+++ GitCapirotescoTest/GitCapirotescoTest/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitCapirotescoTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("O curso é MUITO BRABO!!");
+        }
+    }
+}
